50ddbab7228b58bcce71d6ea20fb66b723176c85
FuncProgLinux <softwarelibre@urutau-ltd.org>
2026-04-23 17:44:20 -0600

    feat(guix): add guix manifest

---
+++ manifest.scm
@@ -0,0 +1,15 @@
+;; Lo que sigue es un "manifest" equivalente a la línea de comando que
+;; introdujo. Puede almacenarlo dentro de un archivo que pudiese pasar a
+;; cualquier comando 'guix' que acepte una opción '--manifest' (o -m).
+
+(specifications->manifest
+ (list "go"
+       "gopls"
+       "govulncheck"
+       "go-staticcheck"
+       "podman"
+       "podman-compose"
+       "make"
+       "git"
+       "gcc-toolchain"
+       "sqlite"))
